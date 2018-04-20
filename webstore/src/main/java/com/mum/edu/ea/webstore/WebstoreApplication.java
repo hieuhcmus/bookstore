f2c45999769d51155c22865599c2f79662b47e6e
hieu <hieu.hcmus@gmail.com>
2018-04-20 11:01:16 -0500

    initial commit

---
+++ webstore/src/main/java/com/mum/edu/ea/webstore/WebstoreApplication.java
@@ -0,0 +1,12 @@
+package com.mum.edu.ea.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstoreApplication.class, args);
+	}
+}
