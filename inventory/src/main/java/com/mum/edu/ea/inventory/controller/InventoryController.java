a443c7f568e7a571d9652a88e7caf0f64764268d
hieu <hieu.hcmus@gmail.com>
2018-04-24 12:20:38 -0500

    remove redundant code

--- inventory/src/main/java/com/mum/edu/ea/inventory/controller/InventoryController.java
+++ inventory/src/main/java/com/mum/edu/ea/inventory/controller/InventoryController.java
@@ -1,11 +1,7 @@
 package com.mum.edu.ea.inventory.controller;
 
 import com.mum.edu.ea.inventory.service.OrderQueueService;
-import com.mum.edu.ea.jms.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +16,5 @@ public class InventoryController {
 	public String prepareProduct(Model model) {
 		model.addAttribute("orders", orderQueueService.getAll());
 		return "index";
-	}
-
-	@SendTo("/topic/new-order")
-	@MessageMapping("/new-order")
-	public Order onNewOrder(Order order) {
-		return order;
 	}
 }
